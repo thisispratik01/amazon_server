abc39edeb3d6a96bd3d937c08708b48d7edc419e
pratik <pratikdhumal2016@gmail.com>
2023-09-20 21:26:41 +0530

    new server

--- middlewares/admin.js
+++ middlewares/admin.js
@@ -7,7 +7,7 @@ const admin=async (req,res,next)=>{
 
         const token=req.header('x-auth-token');
         if(!token){
-          res.status(401).json({msg:"No auth token access denied"});
+         return res.status(401).json({msg:"No auth token access denied"});
       
         }
       
